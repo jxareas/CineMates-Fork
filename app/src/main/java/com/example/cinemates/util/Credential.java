6521ea6d765af9d00d4c1210751ef2097708ecd5
Antonio <tonydinu02@gmail.com>
2021-12-22 12:25:30 +0100

    Chiave API messa in sicurezza

--- app/src/main/java/com/example/cinemates/util/Credential.java
+++ app/src/main/java/com/example/cinemates/util/Credential.java
@@ -1,10 +1,13 @@
 package com.example.cinemates.util;
 
+
+import com.example.cinemates.BuildConfig;
+
 public class Credential {
 
     public static final String BASE_URL = "https://api.themoviedb.org/";
 
-    public static final String API_KEY = "e3769daa94599884c8233ae849529479";
+    public static final String API_KEY = BuildConfig.API_KEY;
 
     public static boolean POPULAR = true;
 
